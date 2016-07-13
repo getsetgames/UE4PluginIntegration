27cc658e8e6b02769a08fb720ad7cbdbdbe4729c
Robert Segal <rob@getsetgames.com>
2016-07-12 22:25:39 -0400

    Initial project commit

---
+++ Source/UE4PluginIntegration/UE4PluginIntegrationGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UE4PluginIntegrationGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4PLUGININTEGRATION_API AUE4PluginIntegrationGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
